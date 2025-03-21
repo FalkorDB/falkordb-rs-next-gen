d8f81f6560637300c76681937ce10212376ffb51
Avi Avni <avi.avni@gmail.com>
2025-03-21 09:51:33 +0200

    fix build

--- graph/build.rs
+++ graph/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-search=/opt/homebrew/opt/llvm/lib");
-    println!("cargo:rustc-link-search=/usr/lib/llvm-19/lib");
+    println!("cargo:rustc-link-search=/usr/lib/llvm-20/lib");
     println!("cargo:rustc-link-lib=omp");
 
     println!("cargo:rustc-link-search=/usr/local/lib");
