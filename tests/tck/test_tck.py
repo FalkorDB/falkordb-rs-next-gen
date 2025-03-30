eddf51d3d70df88cf4a5e01d04b78147874d2790
Avi Avni <avi.avni@gmail.com>
2025-03-30 12:01:44 +0300

    add tck tests

---
+++ tests/tck/test_tck.py
@@ -0,0 +1,40 @@
+import os
+import platform
+import subprocess
+from behave.__main__ import main as behave_main
+import redis
+
+redis_server = None
+client = redis.Redis(protocol=3)
+
+def query(query):
+    return client.execute_command("GRAPH.QUERY", "x", query)
+
+def setup_module(module):
+    global redis_server
+    target = os.environ.get("TARGET", "target/debug/libmatrixdb.dylib" if platform.system() == "Darwin" else "target/debug/libmatrixdb.so")
+    try:
+        client.ping()
+        return
+    except:
+        if os.path.exists("redis-test.log"):
+            os.remove("redis-test.log")
+        redis_server = subprocess.Popen(executable="/usr/local/bin/redis-server", args=["--save", "", "--logfile", "redis-test.log", "--loadmodule", target], stdout=subprocess.PIPE)
+    while True:
+        try:
+            client.ping()
+            return
+        except:
+            pass
+
+
+def teardown_module(module):
+    client.shutdown(nosave=True)
+    redis_server.wait()
+
+
+def test_tck():
+    cmd = ["./tests/tck/features/", '--tags=-crash', '--tags=-skip', "--no-capture"]
+    res = behave_main(cmd)
+    res = 'pass' if res == 0 else 'fail'
+    assert res == 'pass'
