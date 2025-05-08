1966454a3ef441729145f955d2c7d95b8f66fb3c
github-action-benchmark <github@users.noreply.github.com>
2025-05-08 17:41:44 +0000

    add FalkorDB Benchmark (pytest) benchmark result for 5774ecc3229219e498b8a579fc21cbd981b62e14

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746725302079,
+  "lastUpdate": 1746726104620,
   "repoUrl": "https://github.com/FalkorDB/falkordb-rs-next-gen",
   "entries": {
     "FalkorDB Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.06944593012166002",
             "extra": "mean: 4.889389625200005 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "avi.avni@gmail.com",
+            "name": "Avi Avni",
+            "username": "AviAvni"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5774ecc3229219e498b8a579fc21cbd981b62e14",
+          "message": "Merge pull request #61 from FalkorDB/improve-keyword-parsing\n\nimprove keyword parsing",
+          "timestamp": "2025-05-08T20:39:11+03:00",
+          "tree_id": "0bd21e467a87050fed486d0c43783d421a6ea650",
+          "url": "https://github.com/FalkorDB/falkordb-rs-next-gen/commit/5774ecc3229219e498b8a579fc21cbd981b62e14"
+        },
+        "date": 1746726104271,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/bench.py::test_return",
+            "value": 8328.396275264748,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000024455591119047573",
+            "extra": "mean: 120.07113578036504 usec\nrounds: 2261"
+          },
+          {
+            "name": "tests/bench.py::test_unwind",
+            "value": 0.19466840401271499,
+            "unit": "iter/sec",
+            "range": "stddev: 0.047662396614272094",
+            "extra": "mean: 5.136940455599995 sec\nrounds: 5"
           }
         ]
       }
