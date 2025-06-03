937f45ec7ec57f3ac8c7707517491591deeb934d
Avi Avni <avi.avni@gmail.com>
2025-06-03 13:35:41 +0300

    parametrize test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -62,6 +62,21 @@ def is_valid_type(actual_type, expected_type):
     return actual_type & expected_type.__args__[0]
 
 def validate_function(func, expected_args_type):
+    if len(expected_args_type) == 0:
+        query = f"RETURN {func}()"
+        try:
+            common.g.query(query)
+            assert True
+        except Exception as e:
+            assert False, f"Function {func} should accept no arguments but raised an error: {e}"
+        
+        query = f"RETURN {func}(null)"
+        try:
+            common.g.query(query)
+            assert False
+        except Exception as e:
+            assert "Received 1 arguments to function" in str(e), f"Function {func} should not accept null as an argument but raised an error: {e}"
+
     args_type = []
     for _ in range(len(expected_args_type)):
         args_type.append(all_types)
@@ -82,15 +97,17 @@ def validate_function(func, expected_args_type):
                 most = len(expected_args_type)
                 assert f"Received {len(args)} arguments to function '{func}', expected at most {most}" in str(e)
             least = len([x for x in expected_args_type if isinstance(x, Type)])
-            while len(args) >= least:
+            while len(args) > least:
                 args.pop()
-            query = f"RETURN {func}({', '.join(args)})"
-            try:
-                common.g.query(query)
-                assert False
-            except Exception as e:
-                least = len([x for x in expected_args_type if isinstance(x, Type)])
-                assert f"Received {len(args)} arguments to function '{func}', expected at least {least}" in str(e)
+            while len(args) > 0:
+                args.pop()
+                query = f"RETURN {func}({', '.join(args)})"
+                try:
+                    common.g.query(query)
+                    assert False
+                except Exception as e:
+                    least = len([x for x in expected_args_type if isinstance(x, Type)])
+                    assert f"Received {len(args)} arguments to function '{func}', expected at least {least}" in str(e)
         else:
             query = f"RETURN {func}({', '.join(args)})"
             try:
@@ -99,37 +116,41 @@ def validate_function(func, expected_args_type):
             except Exception as e:
                 assert True
 
-def test_functions():
-    validate_function("tointeger", [Type.STRING | Type.BOOL | Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("tofloat", [Type.STRING | Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("tostring", [Type.STRING | Type.INT | Type.BOOL | Type.NULL])
-    validate_function("size", [Type.LIST | Type.STRING | Type.NULL])
-    validate_function("head", [Type.LIST | Type.NULL])
-    validate_function("last", [Type.LIST | Type.NULL])
-    validate_function("tail", [Type.LIST | Type.NULL])
-    validate_function("reverse", [Type.LIST | Type.STRING | Type.NULL])
-    validate_function("substring", [Type.STRING | Type.NULL, Type.INT, Optional[Type.INT]])
-    validate_function("split", [Type.STRING | Type.NULL, Type.STRING | Type.NULL])
-    validate_function("tolower", [Type.STRING | Type.NULL])
-    validate_function("toupper", [Type.STRING | Type.NULL])
-    validate_function("replace", [Type.STRING | Type.NULL, Type.STRING | Type.NULL, Type.STRING | Type.NULL])
-    validate_function("left", [Type.STRING | Type.NULL, Type.INT | Type.NULL])
-    validate_function("ltrim", [Type.STRING | Type.NULL])
-    validate_function("right", [Type.STRING | Type.NULL, Type.INT | Type.NULL])
-    validate_function("string.join", [Type.LIST | Type.NULL, Optional[Type.STRING | Type.NULL]])
-    validate_function("abs", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("ceil", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("exp", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("floor", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("log", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("log10", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("pow", [Type.INT | Type.FLOAT | Type.NULL, Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("round", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("sign", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("sqrt", [Type.INT | Type.FLOAT | Type.NULL])
-    validate_function("range", [Type.INT, Type.INT, Optional[Type.INT]])
-    validate_function("keys", [Type.MAP | Type.NULL])
-    validate_function("toBoolean", [Type.BOOL | Type.STRING | Type.INT | Type.NULL])
+@pytest.mark.parametrize("name, types", [
+    ("tointeger", [Type.STRING | Type.BOOL | Type.INT | Type.FLOAT | Type.NULL]),
+    ("tofloat", [Type.STRING | Type.INT | Type.FLOAT | Type.NULL]),
+    ("tostring", [Type.STRING | Type.INT | Type.BOOL | Type.NULL]),
+    ("size", [Type.LIST | Type.STRING | Type.NULL]),
+    ("head", [Type.LIST | Type.NULL]),
+    ("last", [Type.LIST | Type.NULL]),
+    ("tail", [Type.LIST | Type.NULL]),
+    ("reverse", [Type.LIST | Type.STRING | Type.NULL]),
+    ("substring", [Type.STRING | Type.NULL, Type.INT, Optional[Type.INT]]),
+    ("split", [Type.STRING | Type.NULL, Type.STRING | Type.NULL]),
+    ("tolower", [Type.STRING | Type.NULL]),
+    ("toupper", [Type.STRING | Type.NULL]),
+    ("replace", [Type.STRING | Type.NULL, Type.STRING | Type.NULL, Type.STRING | Type.NULL]),
+    ("left", [Type.STRING | Type.NULL, Type.INT | Type.NULL]),
+    ("ltrim", [Type.STRING | Type.NULL]),
+    ("right", [Type.STRING | Type.NULL, Type.INT | Type.NULL]),
+    ("string.join", [Type.LIST | Type.NULL, Optional[Type.STRING | Type.NULL]]),
+    ("abs", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("ceil", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("e", []),
+    ("exp", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("floor", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("log", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("log10", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("pow", [Type.INT | Type.FLOAT | Type.NULL, Type.INT | Type.FLOAT | Type.NULL]),
+    ("round", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("sign", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("sqrt", [Type.INT | Type.FLOAT | Type.NULL]),
+    ("range", [Type.INT, Type.INT, Optional[Type.INT]]),
+    ("keys", [Type.MAP | Type.NULL]),
+    ("toBoolean", [Type.BOOL | Type.STRING | Type.INT | Type.NULL])
+])
+def test_functions(name, types):
+    validate_function(name, types)
 
 @pytest.mark.extra
 def test_extra_functions():
