8bcf6bf0201142fabea12451c00e7ea7140e6120
Avi Avni <avi.avni@gmail.com>
2025-06-15 12:14:28 +0300

    review

--- fuzz/fuzz_targets/fuzz_target_runtime.rs
+++ fuzz/fuzz_targets/fuzz_target_runtime.rs
@@ -30,7 +30,7 @@ fuzz_target!(|data: &[u8]| -> Corpus {
     unsafe {
         GrB_init(GrB_Mode::GrB_NONBLOCKING as _);
     }
-    let _ = init_functions();
+    init_functions().expect("Failed to init functions");
     let g = RefCell::new(Graph::new(1024, 1024));
 
     std::str::from_utf8(data).map_or(Corpus::Reject, |query| {
