c2210561616654953c90f169c7549f5fb6d1b00a
Barak Bar Orion <barak.bar@gmail.com>
2026-01-25 16:29:52 +0200

    test16_param_introduces_unhandled_type

--- graph/src/cypher.rs
+++ graph/src/cypher.rs
@@ -2397,7 +2397,7 @@ impl<'a> Parser<'a> {
 
     fn parse_node_pattern(
         &mut self,
-        clause: &Keyword,
+        _clause: &Keyword,
     ) -> Result<Arc<QueryNode<Arc<String>, Arc<String>>>, String> {
         match_token!(self.lexer, LParen);
         let alias = if let Ok(id) = self.parse_ident() {
@@ -2408,14 +2408,11 @@ impl<'a> Parser<'a> {
             name
         };
         let labels = self.parse_labels()?;
-        let attrs = if let Token::Parameter(param) = self.lexer.current() {
-            self.lexer.next();
-            if clause == &Keyword::Match {
-                return Err(self
-                    .lexer
-                    .format_error("Encountered unhandled type in inlined properties."));
-            }
-            tree!(ExprIR::Parameter(param))
+        let attrs = if let Token::Parameter(_param) = self.lexer.current() {
+            // Parameters are not allowed as inline properties in any clause
+            return Err(self
+                .lexer
+                .format_error("Encountered unhandled type in inlined properties."));
         } else {
             self.parse_map()?
         };
@@ -2478,14 +2475,11 @@ impl<'a> Parser<'a> {
             } else {
                 None
             };
-            let attrs = if let Token::Parameter(param) = self.lexer.current() {
-                self.lexer.next();
-                if clause == &Keyword::Match {
-                    return Err(self
-                        .lexer
-                        .format_error("Encountered unhandled type in inlined properties."));
-                }
-                tree!(ExprIR::Parameter(param))
+            let attrs = if let Token::Parameter(_param) = self.lexer.current() {
+                // Parameters are not allowed as inline properties in any clause
+                return Err(self
+                    .lexer
+                    .format_error("Encountered unhandled type in inlined properties."));
             } else {
                 self.parse_map()?
             };
